be9cb10f996e297da13bd15691eeff1d7849a5a5
Meltius <bh2593@gmail.com>
2022-10-31 23:24:39 +0900

    10.3.3.2-1

--- Downloads/종합설계/todo-app/src/App.js
+++ Downloads/종합설계/todo-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useCallback } from 'react';
-import TodoTemplate from ''./components/TodoTemplate';
-import TodoInsert from ''./components/TodoInsert';
-import TodoList from ''./components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 
 const App = () => {
@@ -23,21 +23,9 @@ const App = () => {
     },
   ]);
 
-
-
-// 고윳값으로 사용될 id
-  // ref를 사용하여 변수 담기
-  const nextId = useRef(4);
- 
-  const onInsert = useCallback(
-    text => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1; // nextId 1씩 더하기
+const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id != = id));
     },
     [todos],
   );
@@ -47,7 +35,7 @@ const App = () => {
 return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} />
     </TodoTemplate>
   );
 };
