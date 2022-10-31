8f305410a1186918b0ba2418b0fa0f0b351b5a55
Meltius <bh2593@gmail.com>
2022-10-31 23:16:57 +0900

    10.3.1-3

--- Downloads/종합설계/todo-app/components/TodoListItem.js
+++ Downloads/종합설계/todo-app/components/TodoListItem.js
@@ -1,20 +1,25 @@
-import React from 'react‘;
+import React from 'react';
 import {
   MdCheckBoxOutlineBlank,
   MdCheckBox,
   MdRemoveCircleOutline,
-} from 'react-icons/md‘;
+} from 'react-icons/md';
+import cn from 'classnames';
 import './TodoListItem.scss';
 
 
-const TodoListItem = () => {
-  return (
-    <div className="TodoListItem“>
-      <div className=“checkbox“>
-        <MdCheckBoxOutlineBlank />
-        <div className=“text“>할 일</div>
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
+
+
+
+return (
+    <div className="TodoListItem">
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
-      <div className=“remove“>
+      <div className="remove">
         <MdRemoveCircleOutline />
       </div>
     </div>
