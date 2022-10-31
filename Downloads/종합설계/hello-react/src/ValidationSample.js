0adf4bda750eb913d670958fa73c62fb145efcbc
Meltius <bh2593@gmail.com>
2022-10-31 21:40:35 +0900

    5.2.3.1-1

--- Downloads/종합설계/hello-react/src/ValidationSample.js
+++ Downloads/종합설계/hello-react/src/ValidationSample.js
@@ -31,12 +31,10 @@ handleButtonClick => () => {
 render() {
     return (
       <div>
-        <input
-          type=“password“
-          value={this.state.password}
-          onChange={this.handleChange}
-          className={this.state.clicked ? (this.state.validated ? ‘success’ : ‘failure’) : “}
-        />
+      <input
+      ref={(ref) => this.input=ref}
+      (…)
+    />
         <button onClick={this.handleButtonClick}>검증하기</button>
       </div>
     );
