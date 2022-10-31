530bcf2f186e64dda1e225fe971c8221a8e3f8a9
Meltius <bh2593@gmail.com>
2022-10-31 21:53:46 +0900

    8.1-1

--- Downloads/종합설계/hello-react/src/Counter.js
+++ Downloads/종합설계/hello-react/src/Counter.js
@@ -1,16 +1,22 @@
-<button
-  // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
-  onClick={() => {
-    this.setState(
-      {
-        number: number + 1
-      },
-      () => {
-        console.log(‘방금 setState가 호출되었습니다.’);
-        console.log(this.state);
-      }
-    );
-  }}
->
-  +1
-</button>
+import React, { useState } from ‘react‘;
+
+
+const Counter = () => {
+  const [value, setValue] = useState(0);
+
+
+
+return (
+    <div>
+      <p>
+        현재 카운터 값은 <b>{value}</b>입니다.
+      </p>
+      <button onClick={() => setValue(value + 1)}>+1</button>
+      <button onClick={() => setValue(value - 1)}>-1</button>
+    </div>
+  );
+};
+
+
+
+export default Counter;
