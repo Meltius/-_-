a3b10b8a2a5d507793cc4756dc9d1bc122eb3ba1
Meltius <bh2593@gmail.com>
2022-11-10 13:12:45 +0900

    11.1-1

--- Downloads/종합설계/todo-app/src/App.js
+++ Downloads/종합설계/todo-app/src/App.js
@@ -4,42 +4,34 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: </span><span class="cd2 co31">할</span> <span class="cd2 co31">일</span> <span class="cd2 co49">${</span><span class="cd2 co33">i</span><span class="cd2 co49">}</span><span class="cd2 co31">,
       checked: false,
-    },
-  ]);
-  
-  const onToggle = useCallback(
-    id => {
-      setTodos(
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
- 
-  return (
-    <TodoTemplate>
-      <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-    </TodoTemplate>
-  );
+    });
+  }
+  return array;
+}
+
+
+
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
+
+
+
+// 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(2501);
+
+
+
+(…)
 };
- 
+
+
+
 export default App;
