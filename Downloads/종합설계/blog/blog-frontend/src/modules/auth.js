d5fcacfe353ee759d0fcd990bccc50103fbac517
Meltius <bh2593@gmail.com>
2022-11-11 01:15:24 +0900

    24.2.2

--- Downloads/종합설계/blog/blog-frontend/src/modules/auth.js
+++ Downloads/종합설계/blog/blog-frontend/src/modules/auth.js
@@ -1,45 +1,52 @@
-body {
-    margin: 0;
-    padding: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    box-sizing: border-box; /* 엘리먼트의 box-sizing 값을 border-box로 설정 */
-    min-height: 100%;
-  }
-  
-  
-  #root {
-    min-height: 100%;
-  }
-  
-  
-  
-  /* 추후 회원 인증 페이지에서
-   배경 화면을 페이지의 전체 영역에 채우기 위한 용도 */
-  html {
-    height: 100%;
-  }
-  
-  
-  
-  /* 링크의 색상 및 밑줄 없애기 */
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-  
-  
-  
-   {
-    box-sizing: inherit; / 모든 엘리먼트의 box-sizing 값을 border-box로 설정 */
-  }
-  
-  
-  
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
-  }
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+
+
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+
+
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }) => ({
+    form, // register , login
+    key, // username, password, passwordConfirm
+    value, // 실제 바꾸려는 값
+  }),
+);
+export const initializeForm = createAction(INITIALIZE_FORM, form => form); // register / login
+
+
+
+const initialState = {
+  register: {
+    username: ",
+    password: ",
+    passwordConfirm: ",
+  },
+  login: {
+    username: ",
+    password: ",
+  },
+};
+
+
+
+const auth = handleActions(
+  {
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, draft => {
+        draft[form][key] = value; // 예: state.register.username을 바꾼다.
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+    }),
+  },
+  initialState,
+);
+
+
+
+export default auth;
