b2d4c9b69db105f9bbf0817fb78b78606c9db307
Meltius <bh2593@gmail.com>
2022-11-11 00:14:04 +0900

    22.10.5-1

--- Downloads/종합설계/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ Downloads/종합설계/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -21,17 +21,22 @@ export const list = async ctx => {
     // 값이 주어지지 않았다면 1을 기본으로 사용합니다.
     const page = parseInt(ctx.query.page || '1', 10);
   
-    if (page < 1) {
+  
+  if (page < 1) {
       ctx.status = 400;
       return;
     }
   
-    try {
+  
+  
+  try {
       const posts = await Post.find()
         .sort({ _id: -1 })
         .limit(10)
         .skip((page - 1) * 10)
         .exec();
+      const postCount = await Post.countDocuments().exec();
+      ctx.set('Last-Page', Math.ceil(postCount / 10));
       ctx.body = posts;
     } catch (e) {
       ctx.throw(500, e);
