b097beeb78b4608e7c571bcca08dd032da062017
Meltius <bh2593@gmail.com>
2022-11-10 15:59:37 +0900

    17.6.1.1-2

--- Downloads/종합설계/react-redux-tutorial/modules/counter.js
+++ Downloads/종합설계/react-redux-tutorial/modules/counter.js
@@ -1,4 +1,4 @@
-import { createAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -6,29 +6,16 @@ const DECREASE = 'counter/DECREASE';
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-
-
 const initialState = {
-  number: 0
+  number: 0,
 };
 
-
-
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1
-      };
-    default:
-      return state;
-  }
-}
-
-
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
 export default counter;
