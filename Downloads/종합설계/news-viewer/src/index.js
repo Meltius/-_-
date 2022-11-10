b68c41c744e9d078d131154adf4c5685d962d7f1
Meltius <bh2593@gmail.com>
2022-11-10 14:36:51 +0900

    14.7.1-1

--- Downloads/종합설계/news-viewer/src/index.js
+++ Downloads/종합설계/news-viewer/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from ''./NewsItem';
+import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const NewsListBlock = styled.div</span>
+  <span class="co33">(...)</span>
+<span class="co31">;
+
+
+
+const NewsList = ({ category }) => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+
+
+useEffect(() => {
+    // async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === ‘all‘ ? “ : &amp;category=</span><span class="cd2 co49">${</span><span class="cd2 co33">category</span><span class="cd2 co49">}</span><span class="cd2 co31">;
+        const response = await axios.get(
+          https://newsapi.org/v2/top-headlines?country=kr</span><span class="cd2 co49">${</span><span class="cd2 co33">query</span><span class="cd2 co49">}</span><span class="cd2 co31">&amp;apiKey=0a8c4202385d4ec1bb93b7e277b3c51f,
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [category]);
+
+
+
+(…)
+};
+
+
+
+export default NewsList;
