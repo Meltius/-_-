19202dfa41a874d553e5971ca91c2160c4a57c0c
Meltius <bh2593@gmail.com>
2022-11-10 21:46:14 +0900

    18.3.1.5-5

--- Downloads/종합설계/learn-redux-middleware/src/lib/createRequestThunk.js
+++ Downloads/종합설계/learn-redux-middleware/src/lib/createRequestThunk.js
@@ -1,25 +1,32 @@
+import { startLoading, finishLoading } from '../modules/loading';
+
+
 export default function createRequestThunk(type, request) {
-    // 성공 및 실패 액션 타입을 정의합니다.
-    const SUCCESS = </span><span class="co49">${</span><span class="cd2 co34">type</span><span class="co33">}</span><span class="cd2 co31">_SUCCESS;
-    const FAILURE = </span><span class="co49">${</span><span class="cd2 co34">type</span><span class="co33">}</span><span class="cd2 co31">_FAILURE;
-    return params => async dispatch => {
-      dispatch({ type }); // 시작됨
-      try {
-        const response = await request(params);
-        dispatch({
-          type: SUCCESS,
-          payload: response.data
-        }); // 성공
-      } catch (e) {
-        dispatch({
-          type: FAILURE,
-          payload: e,
-          error: true
-        }); // 에러 발생
-        throw e;
-      }
-    };
-  }
-  
-  
-  // 사용법: createRequestThunk('GET_USERS',api.getUsers);
+  // 성공 및 실패 액션 타입을 정의합니다.
+  const SUCCESS = </span><span class="co49">${</span><span class="co34">type</span><span class="co49">}</span><span class="co31">_SUCCESS;
+  const FAILURE = </span><span class="co49">${</span><span class="co34">type</span><span class="co49">}</span><span class="co31">_FAILURE;
+  return params => async dispatch => {
+    dispatch({ type }); // 시작됨
+    dispatch(startLoading(type));
+    try {
+      const response = await request(params);
+      dispatch({
+        type: SUCCESS,
+        payload: response.data
+      }); // 성공
+      dispatch(finishLoading(type));
+    } catch (e) {
+      dispatch({
+        type: FAILURE,
+        payload: e,
+        error: true
+      }); // 에러 발생
+      dispatch(startLoading(type));
+      throw e;
+    }
+  };
+}
+
+
+
+// 사용법: createRequestThunk(‘GET_USERS‘,api.getUsers);
