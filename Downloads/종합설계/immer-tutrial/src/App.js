881e77dc412d8c80b57d749e64882ae1555355fe
Meltius <bh2593@gmail.com>
2022-11-10 13:35:19 +0900

    12.1.4-1

--- Downloads/종합설계/immer-tutrial/src/App.js
+++ Downloads/종합설계/immer-tutrial/src/App.js
@@ -1,9 +1,10 @@
 import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
 
 
 const App = () => {
   const nextId = useRef(1);
-  const [form, setForm] = useState({ name: ", username: " });
+  const [form, setForm] = useState({ name: “, username: “ });
   const [data, setData] = useState({
     array: [],
     uselessValue: null
@@ -15,10 +16,11 @@ const App = () => {
   const onChange = useCallback(
     e => {
       const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name]: [value]
-      });
+      setForm(
+        produce(form, draft => {
+          draft[name] = value;
+        })
+      );
     },
     [form]
   );
@@ -38,71 +40,41 @@ const App = () => {
 
 
   <span class="co44">// array</span><span class="co44">에</span> <span class="co44">새</span> <span class="co44">항목</span> <span class="co44">등록</span>
-  <span class="co47">setData</span><span class="co33">({</span>
-    <span class="co35">...</span><span class="co33">data,</span>
-    import React, { useRef, useCallback, useState } from ‘react‘;
+  <span class="co47">setData</span><span class="co33">(</span>
+    <span class="cd2 co47">produce</span><span class="cd2 co33">(data,</span> <span class="cd2 co33">draft</span> <span class="cd2 co46">=&gt;</span> <span class="cd2 co33">{</span>
+      <span class="cd2 co34">draft</span><span class="cd2 co33">.</span><span class="cd2 co34">array</span><span class="cd2 co33">.</span><span class="cd2 co47">push</span><span class="cd2 co33">(info);</span>
+    <span class="cd2 co33">})</span>
+  <span class="co33">);</span>
+
+  <span class="co44">// form </span><span class="co44">초기화</span>
+  <span class="co47">setForm</span><span class="co33">({</span>
 
 
-    array: data.array.concat(info)
-  });
-
-
-
-<span class="co44">// form </span><span class="co44">초기화</span>
-<span class="co47">setForm</span><span class="co33">({</span>
-
-
-    name: ",
-    username: "
-  });
-  nextId.current += 1;
-},
-[data, form.name, form.username]
-);
+        name: ",
+        username: "
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.username]
+  );
 
 
 
 // 항목을 삭제하는 함수
-const onRemove = useCallback(
-id => {
-  setData({
-    ...data,
-    array: data.array.filter(info => info.id != = id)
-  });
-},
-[data]
-);
+  const onRemove = useCallback(
+    id => {
+      setData(
+        produce(data, draft => {
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+        })
+      );
+    },
+    [data]
+  );
 
 
 
-return (
-<div>
-  <form onSubmit={onSubmit}>
-    <input
-      name="username"
-      placeholder="아이디"
-      value={form.username}
-      onChange={onChange}
-    />
-    <input
-      name="name"
-      placeholder="이름"
-      value={form.name}
-      onChange ={onChange}
-    />
-    <button type="submit">등록</button>
-  </form>
-  <div>
-    <ul>
-      {data.array.map(info => (
-        <li key={info.id} onClick={() => onRemove(info.id)}>
-          {info.username} ({info.name})
-        </li>
-      ))}
-    </ul>
-  </div>
-</div>
-);
+return (...);
 };
 
 
