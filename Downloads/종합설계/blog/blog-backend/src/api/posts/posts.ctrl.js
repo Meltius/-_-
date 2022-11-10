8684ffeb024a4a38a3c44eb8fafa62cde23e553d
Meltius <bh2593@gmail.com>
2022-11-11 00:00:39 +0900

    22.7.1-3

--- Downloads/종합설계/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ Downloads/종합설계/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,20 +1,22 @@
-import Post from '../../models/post';
-
-
-export const write = ctx => {};
-
-
-
-export const list = ctx => {};
-
-
-
-export const read = ctx => {};
-
-
-
-export const remove = ctx => {};
-
-
-
-export const update = ctx => {};
+/
+  POST /api/posts
+  {
+    title: '제목',
+    body: '내용',
+    tags: ['태그1', '태그2']
+  }
+/
+export const write = async ctx => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
