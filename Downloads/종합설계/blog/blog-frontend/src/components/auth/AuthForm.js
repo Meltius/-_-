44d13eeddcc0eafe1700744d6da762e2d4700d8b
Meltius <bh2593@gmail.com>
2022-11-11 01:10:28 +0900

    24.2.1.2-4

--- Downloads/종합설계/blog/blog-frontend/src/components/auth/AuthForm.js
+++ Downloads/종합설계/blog/blog-frontend/src/components/auth/AuthForm.js
@@ -82,6 +82,34 @@ const AuthForm = () => {
   );
 };
 
+const ButtonWithMarginTop = styled(Button)</span>
+  <span class="cd2 co33">margin-top</span><span class="cd2 co34">:</span> <span class="cd2 co32">1rem</span><span class="cd2 co33">;</span>
+<span class="cd2 co31">;
+
+
+const AuthForm = () => {
+  return (
+    <AuthFormBlock>
+      <h3>로그인</h3>
+      <form>
+        <StyledInput autoComplete="username" name="username" placeholder="아이디" />
+        <StyledInput
+          autoComplete="new-password"
+          name="password"
+          placeholder="비밀번호"
+          type="password"
+        />
+        <ButtonWithMarginTop cyan fullWidth>
+          로그인
+        </ButtonWithMarginTop>
+      </form>
+      <Footer>
+        <Link to="/register">회원가입</Link>
+      </Footer>
+    </AuthFormBlock>
+  );
+};
+
 
 
 export default AuthForm;
