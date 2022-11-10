c64a84d490e161d42bb23f0e660c300702bed433
Meltius <bh2593@gmail.com>
2022-11-10 23:32:53 +0900

    21.5.4-2

--- Downloads/종합설계/blog/blog-backend/src/index.js
+++ Downloads/종합설계/blog/blog-backend/src/index.js
@@ -2,31 +2,17 @@ const Koa = require('koa');
 const Router = require('koa-router');
 
 
+const api = require('./api');
+
+
+
 const app = new Koa();
 const router = new Router();
 
 
 
 // 라우터 설정
-router.get('/', ctx => {
-  ctx.body = '홈';
-});
-
-
-
-router.get('/about/:name?', ctx => {
-  const { name } = ctx.params;
-  // name의 존재 유무에 따라 다른 결과 출력
-  ctx.body = name ? </span><span class="co49">${</span><span class="cd2 co33">name</span><span class="co33">}</span><span class="cd2 co31">의</span><span class="cd2 co31"> </span><span class="cd2 co31">소개</span><span class="cd2 co31"> : ‘소개‘;
-});
-
-
-
-router.get('/posts', ctx => {
-  const { id } = ctx.query;
-  // id의 존재 유무에 따라 다른 결과 출력
-  ctx.body = id ? </span><span class="cd2 co31">포스트</span><span class="cd2 co31"> #</span><span class="co49">${</span><span class="cd2 co33">id</span><span class="co49">}</span><span class="cd2 co31"> : ‘포스트 아이디가 없습니다.’;
-});
+router.use('/api', api.routes()); // api 라우트 적용
 
 
 
