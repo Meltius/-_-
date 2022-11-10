7bc2caed648f418cd7d60f2fc8e1e98b3f2b364b
Meltius <bh2593@gmail.com>
2022-11-11 01:27:59 +0900

    24.2.3.5-1

--- Downloads/종합설계/blog/blog-frontend/src/lib/createRequestSaga.js
+++ Downloads/종합설계/blog/blog-frontend/src/lib/createRequestSaga.js
@@ -2,6 +2,14 @@ import { call, put } from 'redux-saga/effects';
 import { startLoading, finishLoading } from '../modules/loading';
 
 
+export const createRequestActionTypes = type => {
+  const SUCCESS = </span><span class="co49">${</span><span class="cd2 co33">type</span><span class="co33">}</span><span class="cd2 co31">_SUCCESS;
+  const FAILURE = </span><span class="co49">${</span><span class="cd2 co33">type</span><span class="co33">}</span><span class="cd2 co31">_FAILURE;
+  return [type, SUCCESS, FAILURE];
+};
+
+
+
 export default function createRequestSaga(type, request) {
   const SUCCESS = </span><span class="co49">${</span><span class="co34">type</span><span class="co49">}</span><span class="co31">_SUCCESS;
   const FAILURE = </span><span class="co49">${</span><span class="co34">type</span><span class="co49">}</span><span class="co31">_FAILURE;
