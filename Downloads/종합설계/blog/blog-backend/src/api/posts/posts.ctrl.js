d8be10f89cc6221b47c12cf6d7d3e2782dfe95a3
Meltius <bh2593@gmail.com>
2022-11-11 00:40:58 +0900

    23.5.5

--- Downloads/종합설계/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ Downloads/종합설계/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -142,4 +142,24 @@ export const read = async ctx => {
     }
   };
   
+  export const getPostById = async (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; // Bad Request
+    return;
+  }
+  try {
+    const post = await Post.findById(id);
+    // 포스트가 존재하지 않을 때
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
   
