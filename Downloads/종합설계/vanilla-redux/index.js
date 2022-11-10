df976d3ea3d74879869880f8c463127c5231e0c8
Meltius <bh2593@gmail.com>
2022-11-10 15:20:27 +0900

    16.2.5-1

--- Downloads/종합설계/vanilla-redux/index.js
+++ Downloads/종합설계/vanilla-redux/index.js
@@ -1,6 +1,6 @@
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
-const btnIncrease = document.querySelector('#id');
+const btnIncrease = document.querySelector('#increase');
 const btnDecrease = document.querySelector('#decrease');
 
 
@@ -14,3 +14,9 @@ const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = difference => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
 
+
+
+const initialState = {
+  toggle: false,
+  counter: 0
+};
