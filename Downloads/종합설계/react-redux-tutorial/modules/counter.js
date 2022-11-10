c35e5be0c736fad846c42316a7e46d58ae75c2bb
Meltius <bh2593@gmail.com>
2022-11-10 16:01:56 +0900

    17.6.1.2-1

--- Downloads/종합설계/react-redux-tutorial/modules/counter.js
+++ Downloads/종합설계/react-redux-tutorial/modules/counter.js
@@ -1,22 +1,10 @@
-import { createAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 
-const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
-const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
-const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제함
-const REMOVE = 'todos/REMOVE'; // todo를 제거함
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
 
-export const changeInput = createAction(CHANGE_INPUT, input => input);
-
-let id = 3; // insert가 호출될 때마다 1씩 더해집니다.
-export const insert = createAction(INSERT, text => ({
-  id: id++,
-  text,
-  done: false,
-}));
-
-export const toggle = createAction(TOGGLE, id => id);
-export const remove = createAction(REMOVE, id => id);
-
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 const initialState = {
   number: 0,
