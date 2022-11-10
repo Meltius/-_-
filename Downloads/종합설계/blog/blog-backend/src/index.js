82783711e6469eea14e2a2bc72cbde877cfb4382
Meltius <bh2593@gmail.com>
2022-11-10 23:31:20 +0900

    21.5.2-1

--- Downloads/종합설계/blog/blog-backend/src/index.js
+++ Downloads/종합설계/blog/blog-backend/src/index.js
@@ -11,8 +11,21 @@ const router = new Router();
 router.get('/', ctx => {
   ctx.body = '홈';
 });
-router.get('/about', ctx => {
-  ctx.body = '소개';
+
+
+
+router.get('/about/:name?', ctx => {
+  const { name } = ctx.params;
+  // name의 존재 유무에 따라 다른 결과 출력
+  ctx.body = name ? </span><span class="co49">${</span><span class="cd2 co33">name</span><span class="co33">}</span><span class="cd2 co31">의</span><span class="cd2 co31"> </span><span class="cd2 co31">소개</span><span class="cd2 co31"> : ‘소개‘;
+});
+
+
+
+router.get('/posts', ctx => {
+  const { id } = ctx.query;
+  // id의 존재 유무에 따라 다른 결과 출력
+  ctx.body = id ? </span><span class="cd2 co31">포스트</span><span class="cd2 co31"> #</span><span class="co49">${</span><span class="cd2 co33">id</span><span class="co49">}</span><span class="cd2 co31"> : ‘포스트 아이디가 없습니다.’;
 });
 
 
@@ -25,4 +38,3 @@ app.use(router.routes()).use(router.allowedMethods());
 app.listen(4000, () => {
   console.log('Listening to port 4000');
 });
-
