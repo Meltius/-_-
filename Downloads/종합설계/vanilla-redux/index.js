defc24fb6347fe461fb6b924cc07a9e62d235547
Meltius <bh2593@gmail.com>
2022-11-10 15:22:57 +0900

    16.2.8-1

--- Downloads/종합설계/vanilla-redux/index.js
+++ Downloads/종합설계/vanilla-redux/index.js
@@ -48,3 +48,21 @@ function reducer(state = initialState, action) {
   }
 }
 import { createStore } from 'redux';
+
+const render = () => {
+    const state = store.getState(); // 현재 상태를 불러옵니다.
+    // 토글 처리
+    if (state.toggle) {
+      divToggle.classList.add(‘active‘);
+    } else {
+      divToggle.classList.remove(‘active‘);
+    }
+    // 카운터 처리
+    counter.innerText = state.counter;
+  };
+  
+  
+  
+  render();
+  
+  
